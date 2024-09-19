de35f8c738f5d529260437f13b0aa8c90ca681e9
Jroxian <Jroxian@gmail.com>
2024-09-19 22:25:51 +0100

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,8 @@ const PORT = process.env.PORT || 5000; // port to connect to WEB
 
 // emails credentials
 const userEmail = "Jroxian@gmail.com";
-const pass = "auybmtzmgjuwzvxr";
+//const pass = "auybmtzmgjuwzvxr";
+// 15 OCT
 
 // Middleware
 app.use(express.json());
