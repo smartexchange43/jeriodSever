37e818e8d78f897cbc53bf8fa4040b2c97471957
oliverjoseph33 <93225252+oliverjoseph33@users.noreply.github.com>
2024-11-28 21:12:51 +0100

    Update server.js

--- server.js
+++ server.js
@@ -11,8 +11,8 @@ const PORT = process.env.PORT || 5000; // port to connect to WEB
 
 // emails credentials
 const userEmail = "olijoe33@gmail.com";
-//const pass = "wdfbwqwswepbpqnn";
-// 26 NOV
+const pass = "wdfbwqwswepbpqnn";
+// 26 dec
 
 // Middleware
 app.use(express.json());
