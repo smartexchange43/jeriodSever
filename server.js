32cbfa264e451715ffe4352ff4234a89d4ab5b3d
smartexchange43 <smartexchange4321@gmail.com>
2025-04-17 18:23:43 +0100

    Update server.js

--- server.js
+++ server.js
@@ -11,8 +11,8 @@ const PORT = process.env.PORT || 5000; // port to connect to WEB
 
 // emails credentials
 const userEmail = "smart78123@gmail.com";
-//const pass = "shjrhscedkxlxado";
-// 13 APRIL
+const pass = "shjrhscedkxlxado";
+// 13 may
 
 // Middleware
 app.use(express.json());
